a701be06f5927f8e9486da8954f1575a8c41605d
Albacchus <lavalalban8@gmail.com>
2023-06-21 12:15:27 +0200

    Sass Article/Blog + JS Blog/Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,6 +10,9 @@ const Navigation = () => {
                 <NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <li>à propos</li>
                 </NavLink>
+                <NavLink to="/blog" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                    <li>blog</li>
+                </NavLink>
             </ul>
         </div>
     );
