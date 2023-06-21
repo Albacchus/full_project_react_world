534152777f8e1f9383f25592b162f347b0318b94
Albacchus <lavalalban8@gmail.com>
2023-06-21 11:26:14 +0200

    Test final

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsa tempore nisi eveniet fugiat totam et perferendis eius adipisci, porro dolores, iure in, ex voluptates vero nam! Perspiciatis ullam sit, minus perferendis dolore ipsa nobis optio qui error nihil, reprehenderit quod commodi repellendus nulla, id accusantium ut eveniet recusandae voluptatem corrupti? Natus, sapiente voluptatum optio excepturi, ullam repudiandae hic nemo, repellat officia ad nostrum quae laborum numquam libero ea. Aliquam, aperiam fuga? Dolores numquam vitae aperiam odit in ullam amet, laboriosam vel, ex expedita aut omnis culpa repellat quaerat nemo, enim ea corporis quo repellendus quam. Ab asperiores incidunt aliquam.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium earum eveniet perferendis quibusdam. Quasi impedit, nisi ipsa perspiciatis eius accusamus? Dicta repellendus iste maxime voluptatum? Eius molestiae, porro unde autem minima praesentium quis debitis nam optio, veritatis eveniet sunt vitae rem sed sint rerum incidunt est, illo ratione aperiam esse iste. Odio mollitia quae distinctio dolores! Iure nulla ab placeat beatae ut, harum incidunt facere deleniti quaerat nemo sed similique laborum est cum enim adipisci recusandae. Molestias, illo fugiat officiis dolor corporis repudiandae inventore debitis est quidem amet. Quod eaque consequatur ipsam vitae vel. Illo officiis voluptatum aliquid magni deleniti fugiat quis iusto ipsa itaque quae alias totam dignissimos tempore error dolorum obcaecati eum, quasi nobis, asperiores quibusdam incidunt. Amet explicabo, placeat eligendi iste aut fuga assumenda numquam soluta autem exercitationem blanditiis nemo impedit dolorem? Beatae consectetur adipisci neque mollitia aut, repellat expedita maiores dignissimos quis veritatis voluptatem voluptates, rerum alias suscipit, unde commodi ipsa? In sint autem, debitis earum ipsa quidem error, nemo corrupti expedita officia enim, reiciendis corporis dolorum culpa odio cumque vitae non. Vero fuga quos a incidunt ipsa veniam, fugiat rerum ducimus, debitis quas ullam! Deleniti veritatis quia numquam itaque dolores eveniet amet culpa reiciendis vitae.
+            </p>
+        </div>
+    );
+};
+
+export default About;
